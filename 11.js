f7d3346a284e5f25cf7e1ed7573f504235c4359b
Valeria Vitvytska <leranmu@gmail.com>
2022-05-14 14:39:14 +0300

    11commit

---
+++ 11.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('form');
+
+const inputs = document.querySelectorAll('input')
+const removeMessage = (elem) => elem.remove();
+
+form.addEventListener('click', (e) => {
+
+	let target = e.target
+
+	target.type === "password" ? target.type = "text" : target.type = "password";
+
+	target.classList.toggle('active');
+
+	let targetAfter = document.querySelectorAll(`${target.dataset.after}`)
+
+	targetAfter.forEach(item => item.classList.toggle('active'));
+
+})
+
+
+form.addEventListener('submit', (e) => {
+
+	e.preventDefault()
+
+	let input1 = document.querySelector('#input-1')
+
+	let input2 = document.querySelector('#input-2')
+
+	input1.value === input2.value && input1.value && input2.value ? alert('You are welcome') : document.body.insertAdjacentHTML('beforeend', '<p class="red">Потрібно ввести однакові значення</p>');
+
+	const redMessage = document.querySelector('.red')
+
+	if (redMessage) {
+		inputs.forEach(input => {
+			input.addEventListener('focus', () => removeMessage(redMessage))
+			input.removeEventListener('focus', () => removeMessage(redMessage))
+
+		})
+	}
+
+	form.reset()
+})
+
+
+
+
+
+
+
+
+
